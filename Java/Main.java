184134614c8376c72f06ee1be9038e4c198902e8
andre3 <andfnts@gmail.com>
2020-12-27 10:36:18 -0600

    Removing old code

--- Java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Display frame = new Display();
-        Display.Window window = frame.new Window();
-    }
-}
--- Java/README.md
+++
@@ -1,2 +0,0 @@
-# Dijkstra
-Decided to make a html/java script version, this java code is no longer being worked on
